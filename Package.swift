f1335f32e9d3a2cf1ee43f2b831bbafd738d036f
fepriyadi <fepriyadi.harahap@mncgroup.com>
2024-07-24 15:38:45 +0700

    update package

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
       // Dependencies declare other packages that this package depends on.
       .package(url: "https://github.com/realm/realm-swift.git", branch: "master"),
       .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
-      .package(path: "https://github.com/fepriyadi/CapstoneCore.git")
+      .package(url: "https://github.com/fepriyadi/CapstoneCore.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
