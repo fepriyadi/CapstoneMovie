672b05769e81d529ca01fb8b4f4a0dffcca1af6c
fepriyadi <fepriyadi.harahap@mncgroup.com>
2024-07-24 15:42:24 +0700

    changes

--- Package.swift
+++ Package.swift
@@ -14,7 +14,6 @@ let package = Package(
     ],
     dependencies: [
       // Dependencies declare other packages that this package depends on.
-      .package(url: "https://github.com/realm/realm-swift.git", branch: "master"),
       .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
       .package(url: "https://github.com/fepriyadi/CapstoneCore.git", branch: "main")
     ],
